9f877dbd1160c285016ca7bc7d77b8ff6f0782fa
Deepak Jena <DeepakRanjanJena131@gmail.com>
2020-07-12 23:07:57 -0400

    PDF export using html2canvas and jspdf first cut

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import jsPDF from 'jspdf';
+import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'jsPDF';
+
+  generatePDF() {
+    const element = document.getElementById('pdf-container');
+    const options = {
+      background: 'white',
+      scale: 3
+    };
+    html2canvas(element, options).then((canvas) => {
+      const img = canvas.toDataURL("image/PNG");
+      const doc = new jsPDF('l', 'mm', 'a4', 1);
+
+      // Add image Canvas to PDF
+      const bufferX = 5;
+      const bufferY = 5;
+      const imgProps = (<any>doc).getImageProperties(img);
+      const pdfWidth = doc.internal.pageSize.getWidth() - 2 * bufferX;
+      const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
+      doc.addImage(img, 'PNG', bufferX, bufferY, pdfWidth, pdfHeight, undefined, 'FAST');
+
+      return doc;
+    }).then((doc) => {
+      doc.save(`sample-${Math.random()}.pdf`);
+    });
+  }
 }
